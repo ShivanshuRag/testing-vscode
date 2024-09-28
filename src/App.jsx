68677d2ff840069a235f9413dbbddab83fb4667c
ShivanshuRag <ragsingh773@gmail.com>
2024-09-28 08:31:59 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
      <h1  className='text-3xl font-bold underline '> Hello World simple</h1>
+      <div className='text-2xl text-orange-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque veniam illo esse provident laborum amet. Ipsa labore repellendus vitae facere . Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus eius aspernatur molestias nemo nesciunt dolore iste, magni modi? Expedita, pariatur nostrum. Laborum excepturi, unde sed eius nostrum esse quas autem consequuntur eligendi corrupti, ea dolor dolorum, consectetur sint fugit tenetur quam neque dolores? Repellat saepe amet eius, fugiat eaque cumque.</div>
     </>
   )
 }
